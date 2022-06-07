14a40742968076686f4fe15a251cf9ce4e61202d
Pierre Carette <pierre.carette78@gmail.com>
2022-06-07 15:53:32 +0200

    the first part is done, i try to display a frontend

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ const app = express()
 require('./config')(app)
 
 // 👇 Start handling routes here
-const index = require('./routes/index')
+const index = require('./routes/index.routes')
 app.use('/', index)
-
+const auth = require('./routes/auth.routes')
+app.use('/auth', auth)
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app)
 
