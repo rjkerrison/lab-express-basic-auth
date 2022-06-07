c8d0539401a35276296ca0b26c5c68199c49c81e
Haroun B <haroun@harryb.dev>
2022-06-07 17:37:10 +0200

    second iteration done

--- routes/signup.route.js
+++ routes/signup.route.js
@@ -3,7 +3,8 @@ const path = require('node:path'),
   User = require('../models/User.model'),
   bcrypt = require('bcryptjs'),
   saltRounds = 10,
-  root = path.normalize(`${__dirname}/..`);
+  root = path.normalize(`${__dirname}/..`),
+  jwt = require('jsonwebtoken');
 
 
 router.route(`/signup`)
@@ -65,7 +66,7 @@ router.route(`/login`)
       const { username, password } = req.body;
       checkCredentialValidity(res, [username, password]);
 
-      
+
       const foundUser = await User.findOne({ username });
       if (!foundUser) {
         res.status(404).json({ message: 'username does not exist' });
@@ -77,14 +78,41 @@ router.route(`/login`)
         res.status(401).json({ message: 'password does not match' });
         return;
       }
-      res
-        .status(200)
-        .json({ isLoggedIn: true, message: 'yayayaya you are ' + username });
+
+      const payload = { username };
+
+      const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+        algorithm: 'HS256',
+        expiresIn: '24h',
+      });
+
+      res.status(200).json({ isLoggedIn: true, authToken });
     } catch (error) {
       console.error(error);
       next(error);
     }
   });
+
+
+// Verify the bearer token is still valid
+router.get('/verify', async (req, res, next) => {
+  try {
+    // get the bearer token from the header
+    const { authorization } = req.headers;
+
+    // isolate the jwt
+    const token = authorization.replace('Bearer ', '');
+
+    // verify the jwt with the jsonwebtoken package
+    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+
+    // send the user the payload
+    res.json({ token, payload })
+  } catch (error) {
+    console.error(error)
+    res.status(400).json({ message: 'Invalid token' })
+  }
+});
 
 
 function checkCredentialValidity(res, credentialArray) {
