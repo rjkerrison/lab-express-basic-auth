74cc31f2d79dcc29a6f449ed17968652c49f199d
Adrien Deveaux <adrien.dvx@gmail.com>
2022-06-07 15:04:49 +0200

    done iteration 1 & beginning of iteration 2

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ require('./config')(app)
 const index = require('./routes/index')
 app.use('/', index)
 
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app)
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,58 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const User = require("../models/User.model.js");
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
 
 /* GET default route */
-router.get('/', (req, res, next) => {
-  res.json({ success: true })
-})
+router.get("/", (req, res, next) => {
+  res.json({ success: true });
+});
 
-module.exports = router
+/* POST / signup */
+router.post("/signup", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const foundUser = await User.findOne({ username });
+    if (foundUser) {
+      res.status(401).json({
+        message: `Seems that you already exist! Maybe you should try to log in instead`,
+      });
+      return;
+    }
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const createdUser = await User.create({
+      username,
+      password: hashedPassword,
+    });
+    res.status(201).json(createdUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* Post login */
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+  const foundUser = await User.findOne({ username });
+
+  if (!foundUser) {
+    res
+      .status(404)
+      .json({ message: "username not found, try to sign in" });
+    return;
+  }
+
+  const isPasswordMatched = await bcrypt.compare(password, foundUser.password);
+  if (!isPasswordMatched) {
+    res
+      .status(401)
+      .json({ message: "password does not match, forgot your password?" });
+    return;
+  }
+  res.status(200).json({ message: "Welcome back " + username });
+});
+
+module.exports = router;
