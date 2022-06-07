1a55bf539cb367a4c796632b0d1efac0c7470df0
Adrien Deveaux <adrien.dvx@gmail.com>
2022-06-07 17:37:23 +0200

    iteration 2done

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const jsonwebtoken = require("jsonwebtoken");
 const User = require("../models/User.model.js");
 const bcrypt = require("bcryptjs");
 const saltRounds = 10;
@@ -35,24 +36,52 @@ router.post("/signup", async (req, res, next) => {
 
 /* Post login */
 router.post("/login", async (req, res, next) => {
-  const { username, password } = req.body;
-  const foundUser = await User.findOne({ username });
+  try {
+    const { username, password } = req.body;
+    const foundUser = await User.findOne({ username });
 
-  if (!foundUser) {
-    res
-      .status(404)
-      .json({ message: "username not found, try to sign in" });
-    return;
-  }
+    if (!foundUser) {
+      res.status(404).json({ message: "username not found, try to sign in" });
+      return;
+    }
 
-  const isPasswordMatched = await bcrypt.compare(password, foundUser.password);
-  if (!isPasswordMatched) {
-    res
-      .status(401)
-      .json({ message: "password does not match, forgot your password?" });
-    return;
+    const isPasswordMatched = await bcrypt.compare(
+      password,
+      foundUser.password
+    );
+    if (!isPasswordMatched) {
+      res
+        .status(401)
+        .json({ message: "password does not match, forgot your password?" });
+      return;
+    }
+
+    const payload = { username };
+    const authToken = jsonwebtoken.sign(payload, process.env.TOKEN_SECRET, {
+      algorithm: "HS256",
+      expiresIn: "3d",
+    });
+    res.status(200).json({ message: "Welcome back " + username, authToken });
+  } catch (err) {
+    next(err);
   }
-  res.status(200).json({ message: "Welcome back " + username });
 });
+
+// Verify 
+
+router.get('/verify', async (req,res,next) => {
+  const {authorization} = req.headers 
+
+  const token = authorization.replace('Bearer ', '')
+
+  try {
+    const payload = jsonwebtoken.verify(token, process.env.TOKEN_SECRET)
+
+    res.json({token, payload})
+  } catch (err){
+    res.status(400).json ({message : 'Invalid token'})
+    next(err)
+  }
+})
 
 module.exports = router;
