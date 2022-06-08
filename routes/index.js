3cd8fd966ecf8806a191bcd236a1df93dcc50a70
caetanolmsouza <caetanomartins@hotmail.com>
2022-06-08 15:38:55 +0200

    configurating heroku

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,7 @@ router.get("/", (req, res, next) => {
   res.json({ success: true });
 });
 
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/cae-labtest";
+
 module.exports = router;
