14dca3c515b8aa7a7e90a351a3df36d5f7f69808
Marly-Yasuke <marly.diallo2021@gmail.com>
2022-08-24 20:19:16 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
 /* GET default route */
 router.get('/', (req, res, next) => {
   res.json({ success: true })
 })
 
+router.use('/auth', require('./user.route'))
+
 module.exports = router
+
+// create main route 
+router.get("/main", isAuthenticated, async (req, res, next) => {
+  try {
+    res.json(
+      "https://media.giphy.com/media/BzyTuYCmvSORqs1ABM/giphy.gif"    );
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/private", isAuthenticated, async (req, res, next) => {
+  console.log("protected route", req.user);
+  try {
+    res.json("https://media.giphy.com/media/y9hjvnO2bwJbO/giphy.gif");
+  } catch (error) {
+    next(error);
+  }
+});
